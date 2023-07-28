ff0599cd085d74b16af8e1b25fbe813f5e5f7618
Gaurav <gaurav@playbook.ai>
2023-07-28 08:19:59 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './css/App.css';
-import 'bootstrap/dist/css/bootstrap.css';
 import { Routes, Route } from 'react-router-dom';
 
-import HomePage from './pages/HomePage';
 import Header from './components/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <>
-    <Header/>
-    <Routes>
-      <Route path="/" element={<HomePage/>}/>
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/contact" element={<Contact />}/>
+      </Routes>
     </>
   );
 }
--- src/pages/HomePage.js
+++
@@ -1,7 +0,0 @@
-export default function HomePage() {
-  return (
-    <div>
-        I am homepage.
-    </div>
-  )
-}
