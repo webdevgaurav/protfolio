5c15eed462e1da2e6f3248730825f9b2f206b387
Gaurav <gaurav@playbook.ai>
2023-08-05 22:03:09 +0530

    updates

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,65 +3,93 @@ import Footer from '../components/Footer';
 import { useState } from 'react';
 
 export default function Contact() {
+
+  const googleSheetUrl = "https://script.google.com/macros/s/AKfycbyUxoXIc0pUy6J_i3Q-L3cvBuTki1lky3nctf78fM4-WzHl5WJg4iyoTf4HNR8xCWsc/exec";
+
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
   const [subject, setSubject] = useState("");
   const [message, setMessage] = useState("");
 
+  const [success, setSuccess] = useState(false);
+
   const handleSubmit = (event) =>{
+    let formEle = document.querySelector("form");
     event.preventDefault();
+    let formData = new FormData(formEle);
+    
     if(name && email && phone) {
-      let contactData = {
-        name: name,
-        email: email,
-        phone: phone,
-        subject: subject,
-        message: message
+
+      var options = {
+          'method': 'POST',
+          'contentType': 'application/json',
+          'body': formData
       };
-      console.log(contactData);
+
+      fetch(googleSheetUrl, options).then(res => res.text())
+      .then(data => {
+        console.log(data)
+        if(data == 'Success') {
+          setSuccess(current => !current);
+          let timer = setInterval(() => {
+            setSuccess(current => !current);
+            clearInterval(timer);
+          }, 2000);
+
+        }
+      })
+      .catch(err => {
+        console.log('Error', err);
+        alert('Something Went Wrong. Please try again!')
+      });
+      
     } else {
       alert('Please fill the required details!')
     }
+
   }
+
   return (
     <>
     <section className="contact" id='contact'>
       <h2 className="heading">Contact <span>Me!</span></h2>
 
-      <form action="submit" className="contact-info">
+      <form className="contact-info">
 
         <div className="input-box">
           <div className="input-field">
-            <input type="text" placeholder='Full Name' onChange={(e) => setName(e.target.value)} required/>
+            <input type="text" placeholder='Full Name' name="name" onChange={(e) => setName(e.target.value)} required/>
             <span className="focus"></span>
           </div>
           <div className="input-field">
-            <input type="email" placeholder='Email' onChange={(e) => setEmail(e.target.value)} required/>
+            <input type="email" placeholder='Email' name="email" onChange={(e) => setEmail(e.target.value)} required/>
             <span className="focus"></span>
           </div>          
         </div>
         
         <div className="input-box">
           <div className="input-field">
-            <input type="number" placeholder='Mobile Number' onChange={(e) => setPhone(e.target.value)}/>
+            <input type="number" placeholder='Mobile Number' name="phone" onChange={(e) => setPhone(e.target.value)}/>
             <span className="focus"></span>
           </div>
           <div className="input-field">
-            <input type="text" placeholder='Email Subject' onChange={(e) => setSubject(e.target.value)} required/>
+            <input type="text" placeholder='Email Subject' name="subject" onChange={(e) => setSubject(e.target.value)} required/>
             <span className="focus"></span>
           </div>
         </div>
 
         <div className="textarea-field">
-          <textarea name="" id="" cols="30" rows="10" placeholder='Your Message' onChange={(e) => setMessage(e.target.value)}></textarea>
+          <textarea name="message" id="" cols="30" rows="10" placeholder='Your Message' onChange={(e) => setMessage(e.target.value)}></textarea>
           <span className="focus"></span>
         </div>
-
+        
         <div className="btn-box btns">
           <button type="submit" className="btn" onClick={handleSubmit}>Submit</button>
         </div>
       </form>
+
+      <div className={success ? "success-message show": "success-message"}><h2>successfully submitted!</h2></div>
     </section>
 
     <Footer />
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -11,28 +11,28 @@ export default function Project() {
             <div className="project-content">
               <div className="content">
                 <h3><i className='bx bx-cog'>&nbsp;</i>The Rethink Academy</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis minus harum incidunt itaque laboriosam cum iste, natus in inventore quo laborum? Dolores quis itaque culpa architecto accusamus unde, eveniet similique!</p>
+                <p>Developed Course module and dashboard.
+                  Wrote reports and had meetings with the management and client to provide
+                  updates on project progress.
+                  Zoom Integration with the Application.
+                  Developed custom APIs that can be integrated into the application.</p>
               </div>
             </div>
 
             <div className="project-content">
               <div className="content">
                 <h3><i className='bx bx-cog'>&nbsp;</i>Excellenc3</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis minus harum incidunt itaque laboriosam cum iste, natus in inventore quo laborum? Dolores quis itaque culpa architecto accusamus unde, eveniet similique!</p>
+                <p>Created several application in the tool Like Project management tool, Store
+                management application, Sample Tracking and Traceablility system and Electrower.
+                Created Dashboard for all the application mentioned.
+                Maintainence and Management of the applications.</p>
               </div>
             </div>
 
             <div className="project-content">
               <div className="content">
                 <h3><i className='bx bx-cog'>&nbsp;</i>Salesforce and Hubspot Integration</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis minus harum incidunt itaque laboriosam cum iste, natus in inventore quo laborum? Dolores quis itaque culpa architecto accusamus unde, eveniet similique!</p>
-              </div>
-            </div>
-
-            <div className="project-content">
-              <div className="content">
-                <h3><i className='bx bx-cog'>&nbsp;</i>Portfolio</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis minus harum incidunt itaque laboriosam cum iste, natus in inventore quo laborum? Dolores quis itaque culpa architecto accusamus unde, eveniet similique!</p>
+                <p>Integrated APIs to connect the applications with the backend. Integrated Salesforce and HubSpot APIs to connect with the application.</p>
               </div>
             </div>
 
